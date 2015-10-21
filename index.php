ffa2bdb9444654f34e44da3a8caf31265dd8c017
dis-zephyro <zephyro@yandex.ru>
2015-10-22 02:05:42 +0300

    v1

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+
+    <head>
+        <?php include('includes/head.php') ?>
+    </head>
+
+    <body>
+        <!-- Header -->
+        <header class="header">
+            <div class="header__top">
+                <div class="container">
+                    <div class="row">
+                        <div class="hidden-xs col-sm-6 col-md-7 col-lg-5">
+                            <ul class="header__link clearfix">
+                                <li class="hidden-sm">
+                                    <a class="header-location" href="#"></a>
+                                </li>
+                                <li class="hidden-sm">
+                                    <a class="header-delivery" href="#"></a>
+                                </li>
+                                <li class="hidden-sm">
+                                    <a class="header-action" href="#"></a>
+                                </li>
+                                <li>
+                                    <a class="header-basket" href="#">3799 руб.</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="hidden-xs col-md-6 col-md-offset-2 col-lg-5 col-lg-offset-5">
+                            <div class="clearfix">
+                                <ul class="header__social hidden-sm clearfix">
+                                    <li>
+                                        <a class="facebook" href="#"></a>
+                                    </li>
+                                    <li>
+                                        <a class="vkontakte" href="#"></a>
+                                    </li>
+                                    <li>
+                                        <a class="instagram" href="#"></a>
+                                    </li>
+                                </ul>
+                                <div class="header__contact">
+                                    <div class="header-tel">+7 (495) 778-24-42</div>
+                                    <div class="header-address"> Павелецкая, ул, Дербеневская
+                                        д.20, стр.12</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-7">
+
+                                </div>
+                                <div class="col-md-8">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="№" class="header__logo">
+                        <img class="img-responsive" src="img/logo.png" alt="Шарофлот - Быстрая доставка воздушных шаров">
+                        <span>Вход</span>
+                    </a>
+                </div>
+            </div>
+            <nav class="header__nav">
+                <div class="container"></div>
+            </nav>
+            <div class="header__content">
+                <div class="container"></div>
+            </div>
+        </header>
+        <!-- Header End-->
+
+        <section class="main">
+            <div class="container">
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <?php include('includes/footer.php') ?>
+        <!-- Footer End-->
+    </body>
+
+</html>
